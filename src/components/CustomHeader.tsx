4ec5fda80b537373519681f0a3a9c15fdf1932b7
jesusinfantes <56434423+jesusinfantes@users.noreply.github.com>
2025-10-19 13:46:58 +0200

    changes

---
+++ src/components/CustomHeader.tsx
@@ -0,0 +1,12 @@
+import { View } from "@aws-amplify/ui-react";
+import "../index.css";
+
+function CustomHeader() {
+    return (
+        <View textAlign="center" padding="0">
+            <h1 className="gradient-text">HABLA</h1>
+        </View>
+    );
+}
+
+export default CustomHeader;
