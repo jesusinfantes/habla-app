6196fcee809e2150789cc9e4ddf35211afa41fa6
jesusinfantes <56434423+jesusinfantes@users.noreply.github.com>
2025-10-21 17:33:16 +0200

    Added isAdmin functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,63 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 const client = generateClient<Schema>();
 
 function App() {
-    const [hablaclasses, setHablaclasses] = useState<Array<Schema["HablaClass"]["type"]>>([]);
+    const [isAdminUser, setIsAdminUser] = useState(false);
+
+    const [hablaclasses, setHablaclasses] = useState<
+        Array<Schema["HablaClass"]["type"]>
+    >([]);
 
     useEffect(() => {
         client.models.HablaClass.observeQuery().subscribe({
             next: (data) => setHablaclasses([...data.items]),
         });
+
+        async function checkAdmin() {
+            try {
+                const { tokens } = await fetchAuthSession();
+                const groups = tokens?.idToken?.payload["cognito:groups"] || [];
+                setIsAdminUser(groups.includes("admin"));
+            } catch (e) {
+                console.log("groups was not an array");
+                setIsAdminUser(false);
+            }
+        }
+
+        checkAdmin();
     }, []);
 
     function createHablaClass() {
         client.models.HablaClass.create({
-           name: window.prompt("Habla Class name"),
-           description: window.prompt("Habla Class description"),
-           teacher: window.prompt("Habla class teacher")
-          });
+            name: window.prompt("Habla Class name"),
+            description: window.prompt("Habla Class description"),
+            teacher: window.prompt("Habla class teacher"),
+        });
     }
 
     const { signOut, user } = useAuthenticator();
 
-    console.log(user);
-    
+    const isAdmin = async () => {
+        try {
+            const { tokens } = await fetchAuthSession();
+            const groups = tokens?.idToken?.payload?.["cognito:groups"] ?? [];
+            return Array.isArray(groups) && groups.includes("admin");
+        } catch (err) {
+            console.warn("No session or failed to fetch auth session:", err);
+            return false;
+        }
+    };
+
     return (
         <main>
             <h1>My Habla Classes</h1>
-            <button onClick={createHablaClass}>+ new</button>
+            {isAdminUser ? (
+                <button onClick={createHablaClass}>+ new class</button>
+            ) : null}{" "}
             <ul>
                 {hablaclasses.map((hablaclass) => (
                     <li key={hablaclass.id}>{hablaclass.name}</li>
